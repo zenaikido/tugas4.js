098c72b9792af14219199495bd7fc35424611f99
Rauf <zen.star12@yahoo.com>
2021-01-31 03:27:02 +0700

    Tugas 4 Javascript Niomic
    Tugas 4 Javascript Niomic

---
+++ tugas4.js
@@ -0,0 +1,14 @@
+var A = 170;
+var B = 180;
+var C = 190;
+
+if(A===C){
+    console.log("A siswa tertinggi");
+    if(A>B){
+        console.log("A siwa tertinggi ke 2");
+    }else{
+    console.log("A siswa terpendek");
+    }
+}else{
+    console.log("A siswa terpendek, B Siswa tertinggi ke 2, C siswa tertinggi");
+}
